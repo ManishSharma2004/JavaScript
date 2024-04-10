111d7b8166a576872f0560cdc8066ff55bbac3d5
Manish Sharma <149959831+ManishSharma2004@users.noreply.github.com>
2024-04-10 20:38:34 +0000

    02

--- Documentation/String.js
+++ Documentation/String.js
@@ -143,3 +143,88 @@ const str2="Sharma"
 // console.log(newstr.includes("I",0));
 // Output : True  // Here position index starts with 0
 
+
+// 10. indexOf()
+
+// This method is used to search strings , and return their indes values
+// It always returns the index of first occurance of searchTerm
+
+// Syntax : indexOf(searchSring)
+// Syntax : indexOf(searchSring, position)
+
+// const tempStr=`I am Manish Sharma`
+// console.log(tempStr.indexOf("am"));
+// // Output : 2
+
+// console.log(tempStr.indexOf(""));
+// // Output : 0 // as null string is passed
+
+// console.log(tempStr.indexOf());
+// // Output : -1 // as nothing is passsed
+
+// console.log(tempStr.indexOf("5"));
+// // Output : -1 // as Not present in the string 
+
+// // If position is greater than the length of the calling string, the method doesn't search the calling string at all. If position is less than zero, the method behaves as it would if position were 0.
+
+// console.log(tempStr.indexOf("am",0));
+// // Output : 2 // As position is less than 2
+
+// console.log(tempStr.indexOf("am",3));
+// // Output : -1 // As position is greter than 2
+
+
+
+// 11. lastIndexOf()
+
+// This methiods find the searchString in the given String , and retuns the index of the last occurrence of it .
+
+// Syntax : lastIndexOf(searchString)
+// Syntax : lastIndexOf(searchString, position)
+
+//  If position is greater than the length of the calling string, the method searches the entire string. If position is less than 0, the behavior is the same as for 0 — that is, the method looks for the specified substring only at index 0.
+
+// const tempstr = "I am Manish Sharma"
+
+// console.log(tempstr.lastIndexOf("m"));
+// // Output : 16
+
+// console.log(tempstr.lastIndexOf(""));
+// // Output : 18  // As it retuns last index so it retuns end null position
+
+// console.log(tempstr.lastIndexOf());
+// // OUtput : -1 // As it undefined
+
+// console.log(tempstr.lastIndexOf("8"));
+// // Output : -1 // As it is not in the String
+
+
+
+// 12. match()
+
+// This method of String values retrieves the result of matching this string against a regular expression.
+
+// It returns An Array whose contents depend on the presence or absence of the global (g) flag, or null if no matches are found.
+
+// Syntax : match(regexp)
+
+const a = ` I am a human , and a student of Computer Scince`
+const b = 'a'
+// console.log(a.match(b))
+// Output : [
+//           'a',
+//          index: 3,
+//          input: ' I am a human , and a student of Computer Scince',
+//          groups: undefined
+//      ]
+
+// const c = a.match(/[A-Z]/g)
+// console.log(c)
+// // Output : [ 'I', 'C', 'S' ]
+
+// const c = a.match(/[0-9]/g)
+// console.log(c)
+// // Output : null // as no number is present 
+
+
+
