7945915a5c7d085d2ec4a207c020dbb4af9ad606
Manish Sharma <149959831+ManishSharma2004@users.noreply.github.com>
2024-04-19 23:54:17 +0530

    Update Array.js

--- Documentation/Array.js
+++ Documentation/Array.js
@@ -906,3 +906,39 @@
 // animals.push(" ")
 // console.log(animals)
 // // // Output : [ 'pigs','goats','sheep','cows','chickens', 'cats','dogs', ' ' ]
+
+
+
+
+
+
+// 28. reduce()
+
+// The reduce() method of Array instances executes a user-supplied "reducer" callback function on each element of the array, in order, passing in the return value from the calculation on the preceding element. The final result of running the reducer across all elements of the array is a single value.
+
+// The first time that the callback is run there is no "return value of the previous calculation". If supplied, an initial value may be used in its place. Otherwise the array element at index 0 is used as the initial value and iteration starts from the next element (index 1 instead of index 0).
+
+// Syntax : reduce(callbackFn)
+// Syntax : reduce(callbackFn, initialValue)
+
+// It returns The value that results from running the "reducer" callback function to completion over the entire array.
+
+// initialValue :-
+
+// A value to which accumulator is initialized the first time the callback is called. If initialValue is specified, callbackFn starts executing with the first value in the array as currentValue. If initialValue is not specified, accumulator is initialized to the first value in the array, and callbackFn starts executing with the second value in the array as currentValue. In this case, if the array is empty (so that there's no first value to return as accumulator), an error is thrown.
+
+// Code :-
+
+// const array1 = [1, 2, 3, 4];
+
+// // 0 + 1 + 2 + 3 + 4 
+
+// const initialValue = 0;
+// const sumWithInitial = array1.reduce(
+//   (accumulator, currentValue) => accumulator + currentValue,
+//   initialValue,
+// );
+
+// console.log(sumWithInitial);
+// Output : 10
+
