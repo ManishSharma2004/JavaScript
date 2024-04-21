776a94249ec5e5dc79ea6a78cdc496ff1e445fc2
Manish Sharma <149959831+ManishSharma2004@users.noreply.github.com>
2024-04-21 23:27:51 +0530

    Update Array.js

--- Documentation/Array.js
+++ Documentation/Array.js
@@ -1,3 +1,5 @@
+// 
+
 // ----------------------------Array------------------------------ // 
 
 // The Array object, as with arrays in other programming languages, enables storing a collection of multiple items under a single variable name, and has members for performing common array operations
